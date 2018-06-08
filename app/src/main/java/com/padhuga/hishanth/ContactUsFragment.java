9855ce12a476ea63db973667001df4d57b3e4379
bhaarn <bharani.mars@gmail.com>
2018-06-08 17:14:23 +0100

    Added google maps

--- app/src/main/java/com/padhuga/hishanth/ContactUsFragment.java
+++ app/src/main/java/com/padhuga/hishanth/ContactUsFragment.java
@@ -2,13 +2,28 @@ package com.padhuga.hishanth;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class ContactUsFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ContactUsFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap googleMap;
+    MapView mapView;
+    View rootView;
+
     public static ContactUsFragment newInstance() {
         return new ContactUsFragment();
     }
@@ -17,7 +32,7 @@ public class ContactUsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_contact_us, container, false);
+        rootView = inflater.inflate(R.layout.fragment_contact_us, container, false);
         TextView mobileNumber = rootView.findViewById(R.id.mobile_number);
         mobileNumber.setOnClickListener(viewClickListener);
         TextView emailId = rootView.findViewById(R.id.email_id);
@@ -40,4 +55,25 @@ public class ContactUsFragment extends Fragment {
             }
         }
     };
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView = (MapView) rootView.findViewById(R.id.mapView);
+        if(mapView != null) {
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        this.googleMap = googleMap;
+        this.googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(9.962876,78.131009)).title("Hisanth Photography Office").snippet("You can see us there"));
+        CameraPosition cameraPosition = CameraPosition.builder().target(new LatLng(9.962876,78.131009)).zoom(16).bearing(0).tilt(45).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
 }
